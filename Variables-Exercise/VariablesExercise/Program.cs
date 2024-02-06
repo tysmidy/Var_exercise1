458317afd8bcbfd81318083f57c98eeb9608c91f
Tyson <tsmithj418@gmail.com>
2024-02-06 09:20:21 -0600

    init commit

---
+++ Variables-Exercise/VariablesExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            string myName = "tyson";
+            int myAge = 23;
+            char myLetter = 't';
+            bool imTired = true;
+            double streetNum = 450;
+            decimal changeJar = 4156;
+            Console.WriteLine("Hello, My name is {0}. \n I'm {1} years old. ", myName ,myAge);
+
+
+        }
+    }
+}
